65f9468ac7fef148373a28509301b6cbe1b29f37
Ifeoluwa <ifel1939@gmail.com>
2022-11-12 18:48:57 +0100

    search functionality implemented

--- imageFilter.js
+++ imageFilter.js
@@ -1,25 +1,42 @@
+const buttons = document.querySelectorAll(".btn");
+const images = document.querySelectorAll(".all-animals");
+const input = document.querySelector("input");
 
-const buttons=document.querySelectorAll(".btn")
-const images=document.querySelectorAll(".store-items")
+//filtering using categories button
+buttons.forEach(function (button) {
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+    const filteredImage = e.target.dataset.filter;
+    console.log(filteredImage);
 
-buttons.forEach(function(button){
+    images.forEach((image) => {
+      if (filteredImage == "all") {
+        image.style.display = "block";
+      } else if (image.classList.contains(filteredImage)) {
+        image.style.display = "block";
+      } else {
+        image.style.display = "none";
+      }
+    });
+  });
+});
 
-    button.addEventListener("click",(e)=>{
-e.preventDefault()
-        const filteredImage=e.target.dataset.filter
-        console.log(filteredImage)
+input.addEventListener("keyup", () => {
+  const inputValue = input.value.trim(); //remove white space
+  if (inputValue.charAt(0) == "") return; //return if first type letter is whitespace
+  images.forEach((image) => {
+    if (image.classList.contains(inputValue)) {
+      image.style.display = "block";
+    } else {
+      image.style.display = "none";
+    }
+  });
+});
 
-images.forEach((image)=>{  
-if(filteredImage=="all"){image.style.display="block"}
-else if(image.classList.contains(filteredImage)){image.style.display="block"}
-else{image.style.display="none"}
-
-
-
-})
- 
-
-
-    })
-
-}) 
+input.addEventListener("input", (e) => {
+  if (input.value.trim() == "") {
+    images.forEach((image, index) => {
+      images[index].style.display = "block";
+    });
+  }
+});
